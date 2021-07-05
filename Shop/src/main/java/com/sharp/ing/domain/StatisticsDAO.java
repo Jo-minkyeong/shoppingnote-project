dc90696f0192991d7ac00dde271f168d7ce51a5e
minkyeong <whalsrud0819@gmail.com>
2021-07-05 18:56:06 +0900

    210705

---
+++ Shop/src/main/java/com/sharp/ing/domain/StatisticsDAO.java
@@ -0,0 +1,30 @@
+package com.sharp.ing.domain;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository("StatisticsDAO")
+public interface StatisticsDAO {
+	
+	Logger logger = LoggerFactory.getLogger("com.sharp.ing.domain.StatisticsDAO");
+	
+	// 전체 소비 평균 값
+	public List<Shopping_listDTO> totalAvg() throws Exception;
+	
+	// 사용자 소비 평균 값
+	public List<Shopping_listDTO> userAvg(String userId) throws Exception;
+
+	//카테고리별 통계
+	public List<CategoryAvgDTO> categoryAvg3(String userId) throws Exception;
+	
+	//카테고리별 6개월 통계
+	public List<CategoryAvgDTO> categoryAvg6(String userId) throws Exception;
+	
+	//카테고리별 12개월 통계
+	public List<CategoryAvgDTO> categoryAvg12(String userId) throws Exception;
+}
